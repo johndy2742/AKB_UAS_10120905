549bc8d5a0341cb184a9a6727dbdaf3413de8c68
johndy <johndypanca12345@gmail.com>
2023-08-07 00:44:45 +0700

    add navbar color

--- app/src/main/java/com/example/uas_10120905_johndypanca/MainActivity.kt
+++ app/src/main/java/com/example/uas_10120905_johndypanca/MainActivity.kt
@@ -81,7 +81,7 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main, menu)
+        menuInflater.inflate(R.menu.activity_main_drawer, menu)
         return true
     }
 
